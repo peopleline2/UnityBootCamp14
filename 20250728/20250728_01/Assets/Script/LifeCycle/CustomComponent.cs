969918d400b5faeeea1d93e10e635a7086c35802
peopleline2 <peopleline010@gmail.com>
2025-07-28 12:28:25 +0900

    2025-07-28 월
    오전) 유니티 스크립트(생명 주기)

---
+++ 20250728/20250728_01/Assets/Script/LifeCycle/CustomComponent.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class CustomComponent : MonoBehaviour
+{
+   public string message;
+}
---
+++ 20250728/20250728_01/Assets/Script/LifeCycle/Sample1.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Sample1 : MonoBehaviour
+{
+    public CustomComponent cc;
+
+
+    private void Awake()
+    {
+        cc = GetComponent<CustomComponent>();
+    }
+}
